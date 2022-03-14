cef5c5e355e82006be5ac1d846c54d963d0130d3
Siro_256 <siro@siro256.dev>
2022-03-14 10:09:06 +0900

    Setup root build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.6.10"
 }
 
 group = "dev.siro256.issue_sample"
-version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+allprojects {
+    apply(plugin = "kotlin")
 
-dependencies {
-    implementation(kotlin("stdlib"))
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        maven { url = uri("https://repo.siro256.dev/repository/maven-public/") }
+    }
+
+    tasks {
+        withType<KotlinCompile> {
+            kotlinOptions.apply {
+                allWarningsAsErrors = true
+                jvmTarget = "17"
+            }
+
+            sourceCompatibility = "17"
+            targetCompatibility = "17"
+        }
+
+        withType<ProcessResources> {
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+            from(rootProject.file("LICENSE").path)
+            rename { "LICENSE_${rootProject.name}" }
+        }
+    }
+
+    configurations.all {
+        resolutionStrategy.cacheChangingModulesFor(0, TimeUnit.SECONDS)
+    }
 }
