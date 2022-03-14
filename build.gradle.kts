a3d8e1048c7a777458f2614cd8a8e584f0cdff5d
Siro_256 <siro@siro256.dev>
2022-03-14 10:08:38 +0900

    Setup Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "dev.siro256.issue_sample"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
