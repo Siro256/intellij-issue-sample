80325cf721ca177a90377e34d90775d0e88c219a
Siro_256 <siro@siro256.dev>
2022-03-14 10:13:53 +0900

    Use kotlin plugin for all modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://repo.siro256.dev/repository/maven-public/") }
+    }
+
+    plugins {
+        kotlin("jvm") version "1.6.10"
+    }
+}
+
 rootProject.name = "intellij-issue-sample"
 
