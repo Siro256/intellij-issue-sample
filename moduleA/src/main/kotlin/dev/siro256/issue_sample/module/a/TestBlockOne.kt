afa533373a1168d48dd01ff6210b9838c5ff24bb
Siro_256 <siro@siro256.dev>
2022-03-14 11:03:16 +0900

    Create test blocks

---
+++ moduleA/src/main/kotlin/dev/siro256/issue_sample/module/a/TestBlockOne.kt
@@ -0,0 +1,6 @@
+package dev.siro256.issue_sample.module.a
+
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.material.Material
+
+object TestBlockOne: Block(Properties.of(Material.STONE))
---
+++ moduleB/src/main/kotlin/dev/siro256/issue_sample/module/b/TestBlockTwo.kt
@@ -0,0 +1,6 @@
+package dev.siro256.issue_sample.module.b
+
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.material.Material
+
+object TestBlockTwo: Block(Properties.of(Material.STONE))
